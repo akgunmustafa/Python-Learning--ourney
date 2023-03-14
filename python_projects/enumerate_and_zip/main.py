3ac3a633dffdb7b3f9edd7e708c43d2af7ee2724
akgunmustafa <makgun095@gmail.com>
2023-03-14 21:42:05 +0300

    Python-Learning-Journey

---
+++ python_projects/enumerate_and_zip/main.py
@@ -0,0 +1,13 @@
+# Enumerate
+
+names = ['Tyler', 'Dias', 'Cory', 'Cameron']
+for i, e in enumerate(names, start=3):
+    print(i, "indexing : ", e)
+
+# Zip()
+
+sale_price = [3500.00, 76300.00, 67200.00]
+cost_price = [2445.00, 45789.00, 12256.00]
+for s, c in zip(sale_price, cost_price):
+    profit = s - c
+    print(f'Total profit : {profit}')
